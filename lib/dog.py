1ae90c91832590a9da4db5b71d4ded940ddc106a
Elly <ellyjames1999@gmail.com>
2025-02-28 14:47:02 +0300

    passed all tests

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,35 @@
 from models import Dog
 
-def create_table(base):
-    pass
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from lib.models import Base, Dog
 
+# Function to create the table
+def create_table(base, engine):
+    base.metadata.create_all(engine)
+
+# Function to save a Dog instance
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
 
+# Function to get all Dog instances
 def get_all(session):
-    pass
+    return session.query(Dog).all()
 
+# Function to find a Dog by name
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter_by(name=name).first()
 
+# Function to find a Dog by ID
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter_by(id=id).first()
 
+# Function to find a Dog by name and breed
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog).filter_by(name=name, breed=breed).first()
 
-def update_breed(session, dog, breed):
-    pass
+# Function to update a Dog's breed
+def update_breed(session, dog, new_breed):
+    dog.breed = new_breed
+    session.commit()
--- lib/models.py
+++ lib/models.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import (Column, String, Integer)
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
 class Dog(Base):
     __tablename__ = 'dogs'
-
-    id = Column(Integer(), primary_key=True)
-    name = Column(String())
-    breed = Column(String())
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    breed = Column(String, nullable=False)
